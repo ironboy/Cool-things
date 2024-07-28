507d751e327f7d07b5fb19b879ccd8cc6e922999
Ironboy <ironboy@users.noreply.github.com>
2024-07-28 13:17:34 +0200

    auto-kill processes in the way of our ports

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { reactEasierViteConfig as revc_ } from 'react-easier/vite-config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { killPortProcess } from 'kill-port-process';
+
+// vite standard port is 5173 - kill anything on that port
+await killPortProcess(5173);
 
 // https://vitejs.dev/config/
 export default defineConfig(revc_({
