0fa3563b5cf01cb8fd83093119ae9c5332785bc5
Ironboy <ironboy@users.noreply.github.com>
2025-03-14 06:20:52 +0100

    multioshack

--- dist/productImages/hej.js
+++ dist/productImages/hej.js
@@ -1,11 +1,34 @@
 import { exec } from 'child_process';
 
+
+
 // The shell command you want to to run
-let command = 'dir ..';
+
+// check which os
+// possible values 'aix', 'darwin', 'freebsd', 'linux',
+// 'openbsd', 'sunos', 'win32' (for all windows variants)
+let os = process.platform;
+
+// Decide on a command to run
+// Note: The command will be run in the bash shell onn Linux, Darwin etc
+// but in Windows will be run by CMD (not PowerShell!)
+let command;
+if (os === 'win32') {
+  command = 'dir ..';
+}
+else {
+  command = 'ls -la ..'
+}
+
+
+
+// YOU DON'T NEED TO CHANGGE THE CODE BELOW!
 
 // Function that executes the command
 function execFunc() {
   console.log('<pre>');
+  console.log('OS ON THE SERVER:', os, '\n');
+  console.log('COMMAND:', command, '\n\n');
   return new Promise((resolve, _reject) => {
     let process = exec(command);
     process.stdout.on('data', data => console.log('stdout:', fix(data)));
@@ -19,6 +42,6 @@ function execFunc() {
 await execFunc();
 
 // function to replace < and > with &lt; and &gt; + strange space char to normal
-function fix(str){
-  return str.replaceAll('<','&lt;').replaceAll('>','&gt;').replaceAll('�',' ');
+function fix(str) {
+  return str.replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('�', ' ');
 }
